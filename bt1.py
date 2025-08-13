1ab39b9086f78d074f936bf2c12c4ce26ae88a12
Tuan Duy <tranquoctuanduy@gmail.com>
2025-08-13 21:53:54 +0700

    them 1 file bai tap thuc hanh

---
+++ bt1.py
@@ -0,0 +1,9 @@
+import math
+for i in range(2,101) :
+    is_prime=True
+    for j in range(2,int(math.sqrt(i))+1):
+        if i%j == 0:
+            is_prime=False
+            break
+    if is_prime==True:
+        print(i)
